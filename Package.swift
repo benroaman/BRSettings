05af1b4e4bfa44caa9c04d9d5808a52449cda6ed
Ben Roaman <benroaman@Bens-MacBook-Pro-3.local>
2025-03-24 13:18:51 -0400

    initial commit

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BRSettings",
+    platforms: [.macOS(.v12),
+                .iOS(.v15),
+                .watchOS(.v8),
+                .macCatalyst(.v15),
+                .tvOS(.v15),
+                .visionOS(.v1)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "BRSettings",
+            targets: ["BRSettings"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "BRSettings"),
+        .testTarget(
+            name: "BRSettingsTests",
+            dependencies: ["BRSettings"]),
+    ]
+)
